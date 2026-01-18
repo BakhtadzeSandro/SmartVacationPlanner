be84b1dda68e8ce20860032617769f0f8a3d7dc7
BakhtadzeSandro <sandrobakhtadze1909@gmail.com>
2026-01-18 15:31:08 +0400

    feat: init monorepo

---
+++ apps/ui/src/app/app.ts
@@ -0,0 +1,21 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Message } from '@smart-vacation-planner/shared-types';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('ui');
+  constructor(){
+    const message: Message = {
+      title: 'Hello',
+      content: 'World',
+      createdAt: new Date()
+    }
+    console.log(message);
+  }
+}
