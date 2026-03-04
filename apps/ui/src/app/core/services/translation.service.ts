8920437fdae532c6f245e5562a136d7f6604a942
BakhtadzeSandro <sandrobakhtadze1909@gmail.com>
2026-03-05 00:58:56 +0400

    feat: save selected language in ls

--- apps/ui/src/app/core/services/translation.service.ts
+++ apps/ui/src/app/core/services/translation.service.ts
@@ -1,14 +1,25 @@
 import { Injectable, inject, signal } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
+const LANG_KEY = 'lang';
+
 @Injectable({ providedIn: 'root' })
 export class TranslationService {
   private readonly translate = inject(TranslateService);
 
-  readonly currentLang = signal(this.translate.getCurrentLang() || 'en');
+  readonly currentLang = signal(this.getInitialLang());
+
+  constructor() {
+    this.translate.use(this.currentLang());
+  }
 
   switchLanguage(lang: string): void {
     this.translate.use(lang);
     this.currentLang.set(lang);
+    localStorage.setItem(LANG_KEY, lang);
+  }
+
+  private getInitialLang(): string {
+    return localStorage.getItem(LANG_KEY) || this.translate.getCurrentLang() || 'en';
   }
 }
